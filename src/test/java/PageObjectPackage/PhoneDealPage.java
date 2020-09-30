f515a16914dd73d1e6d0d6563942718229946b2b
bhupendra2010 <58640548+bhupendra2010@users.noreply.github.com>
2020-09-30 21:37:22 +0100

    Update PhoneDealPage.java

--- src/test/java/PageObjectPackage/PhoneDealPage.java
+++ src/test/java/PageObjectPackage/PhoneDealPage.java
@@ -14,5 +14,6 @@ public class PhoneDealPage extends DriverFactory {
     public void simonlydeal(){
         driver.findElement(By.xpath("//button[contains(text(),'No')]")).click();
         driver.findElement(By.linkText("Family SIM deals")).click();
+        /*new project*/
     }
 }
